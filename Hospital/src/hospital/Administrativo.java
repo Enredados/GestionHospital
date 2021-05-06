64227487338b1f0673ba0daaf57485de6626ca46
xlcaceres <lizxime4ever@hotmail.com>
2021-05-05 23:41:04 -0500

    Update Administrativo.java

--- Hospital/src/hospital/Administrativo.java
+++ Hospital/src/hospital/Administrativo.java
@@ -5,6 +5,9 @@
  */
 package hospital;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
 /**
  *
  * @author alang
@@ -16,6 +19,9 @@ public class Administrativo extends Persona {
     public Administrativo(String id, String nombre, int edad, char genero, String contrasena) {
         super(id, nombre, edad, genero);
         this.contrasena = contrasena;
+    }
+    public Administrativo(){
+        
     }
 
     public String obtenerContrasena() {
@@ -29,5 +35,63 @@ public class Administrativo extends Persona {
     public String obtenerInformacion() {
         return "ID:" + id + ";Nombre:" + nombre + ";Edad:" + edad
                 + ";Género:" + genero + ";Contraseña:" + contrasena;
+    }
+    
+    public void guardar() {
+        String raiz = System.getProperty("user.dir");
+        try {
+            File arch = new File(raiz + "\\ADMINISTRATIVO.dat");
+            RandomAccessFile archivo = new RandomAccessFile(arch, "rw");
+            darFormato();
+
+            if (archivo.length() != 0) {
+                archivo.seek(archivo.length());
+                archivo.writeUTF(id);
+                archivo.writeUTF(nombre);
+                archivo.writeInt(edad);
+                archivo.writeChar(genero);
+                archivo.writeUTF(contrasena);
+                archivo.close();
+            } else {
+                archivo.writeUTF(id);
+                archivo.writeUTF(nombre);
+                archivo.writeInt(edad);
+                archivo.writeChar(genero);
+                archivo.writeUTF(contrasena);
+                archivo.close();
+            }
+        } catch (IOException e) {
+            System.out.println("Error en el ingreso");
+        }
+
+    }
+    
+    public void darFormato(){
+        if(id.length() < 5){
+           for(int i = id.length(); i < 5; i++){
+                id +=" ";
+            }
+        }
+        else {
+            id = id.substring(0,5);
+        }
+        
+        if(nombre.length() < 25){
+           for(int i = nombre.length(); i < 25; i++){
+                nombre +=" ";
+            }
+        }
+        else {
+            nombre = nombre.substring(0,25);
+        }
+        
+        if(contrasena.length() < 10){
+           for(int i = contrasena.length(); i < 10; i++){
+                contrasena +=" ";
+            }
+        }
+        else {
+            contrasena = contrasena.substring(0,10);
+        }
     }
 }
